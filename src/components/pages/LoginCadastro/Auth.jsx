ec70cdb591c59e42732beff3e6795e94d01157b6
Matosaz <mathusphd20@gmail.com>
2024-09-09 12:06:45 -0300

    Atualização

--- src/components/pages/LoginCadastro/Auth.jsx
+++ src/components/pages/LoginCadastro/Auth.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Auth.css';
 import LoginVideo from './LoginBackground.mp4'
-
+import LogoNeotech from "../../../assets/Logo.png";
 const Auth = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
   const [formData, setFormData] = useState({
@@ -39,6 +39,7 @@ const Auth = () => {
     <div className="auth-container">
       <video autoPlay loop playsInline className="VideoBackground"src={LoginVideo}></video>
       <div className="auth-box">
+      <img className="LogoNeotech"src={LogoNeotech} alt="Logo da Neotech" />
       <h2 className='boasvindas'>{isLoginMode ? 'Bem vindo de volta!' : 'Bem vindo!'}</h2>
       
       
