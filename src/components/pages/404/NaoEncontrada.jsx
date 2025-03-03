d2f8318f3c272bb0748237052a1bae0181d510d2
Matosaz <mathusphd20@gmail.com>
2025-03-03 17:38:20 -0300

    Not found page developed

--- src/components/pages/404/NaoEncontrada.jsx
+++ src/components/pages/404/NaoEncontrada.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './NaoEncontrada.css'
 import { ArrowBack } from '@mui/icons-material';
-
+import cat404 from '../../../assets/error404-2.gif';
 const NaoEncontrada = () => {
 
   const Voltar =() =>{
@@ -9,10 +9,12 @@ const NaoEncontrada = () => {
   }
   return ( 
     <div className='ErrorNotFound'>
+   
+      <img className="cat404"src={cat404} alt="" />
+      <h1 className='Errorh1'> Ooooooops...</h1>
+      <h3 className='Errorh3'>Aparentemente sua página não foi encontrada! Tente retornar para a página anterior</h3>
       <button onClick={Voltar} className='voltar'>   
       <ArrowBack fontSize="small" className="icon" />Retornar</button>
-      <h1 className='Errorh1'> Ops...</h1>
-      <h3 className='Errorh3'>Aparentemente sua página não foi encontrada! Tente retornar para a página anterior</h3>
       </div>
 
   )
